4eeac526fb02b423c40735ed0fc5ee60561231f9
Ridhi585 <ridhitrehan585@gmail.com>
2025-11-29 00:06:07 +0530

    Add files via upload

---
+++ kangaroo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class kangaroo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int v1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int v2 = sc.nextInt();
+
+        if (x1 == x2) {
+            System.out.println("YES");
+        } else if (x1 < x2 && v1 > v2) {
+           int r1=(x2 - x1) % (v1 - v2);
+           if (r1==0){
+               System.out.println("YES");
+           }else {
+               System.out.println("NO");
+           }
+        } else if (x2 < x1 && v2 > v1) {
+           int r2=(x1 - x2) % (v2 - v1);
+            if (r2==0){
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
